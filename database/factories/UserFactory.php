472f195674efe496b6fe586c4c68ee7b2bcb0451
unknown <mailstokindle@gmail.com>
2024-07-06 16:01:23 +0100

    add test for adding user to an organisation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'phone' => '08100000000',
+            'phone' => fake()->phoneNumber(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
--- tests/Feature/OrganisationUserTest.php
+++ tests/Feature/OrganisationUserTest.php
@@ -1,4 +1,29 @@
 <?php
 
+use App\Models\Organisation;
+use App\Models\User;
 
-test('user can add another user to organisation', function () {});
+test('user can add another user to an organization', function () {
+    $user = User::factory()->create();
+
+    $organisation = Organisation::factory()->create();
+    $organisation->members()->attach($user);
+
+    $userToAdd = User::factory()->create();
+
+    // Authenticate the user who will add another user to the organization
+    $this->actingAs($user)->postJson('api/organisations/' . $organisation->id . '/users', [
+        'userId' => $userToAdd->id,
+    ])
+        ->assertStatus(200)
+        ->assertJson([
+            'status' => 'success',
+            'message' => 'User added to organisation successfully',
+        ]);
+
+    // Assert that the user is now associated with the organization
+    $this->assertDatabaseHas('organisation_user', [
+        'organisation_id' => $organisation->id,
+        'user_id' => $userToAdd->id,
+    ]);
+});
