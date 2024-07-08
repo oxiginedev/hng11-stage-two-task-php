60143157895570a02c0e854812b46844b5688128
unknown <mailstokindle@gmail.com>
2024-07-08 14:34:36 +0100

    fix authentication failed exception

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,15 +13,11 @@ class LoginController
     public function login(Request $request): JsonResponse
     {
         $input = Validator::make($request->input(), [
-            'email' => ['required', 'string', 'email:filter', 'exists:users,email'],
+            'email' => ['required', 'string', 'email:filter'],
             'password' => ['required', 'string', 'max:255'],
         ])->validate();
 
-        if (! $token = auth()->attempt($input)) {
-            throw new BadRequestHttpException(
-                message: 'Authentication failed',
-            );
-        }
+        abort_if(! $token = auth()->attempt($input), 401, 'Authentication failed');
 
         return response()->json([
             'status' => 'success',
